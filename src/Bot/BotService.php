02258cb95644547c3c2a276b7557cd25b6172234
Marko Mitranic <marko.mitranic@catenamedia.com>
2018-04-16 01:20:14 +0200

    Allow real DI autowiring on Telegram Service

--- src/Bot/BotService.php
+++ src/Bot/BotService.php
@@ -4,13 +4,11 @@ namespace App\Bot;
 
 use App\Logger;
 use Longman\TelegramBot\Exception\TelegramException;
-use Longman\TelegramBot\Request;
 use Longman\TelegramBot\TelegramLog;
 
 class BotService
 {
 
-    const BOT_API_KEY = '586818585:AAFz5J_rX2zU4fVe8RfyO3xVqwCr9N-FUZA';
     const WEBHOOK_SET_ALLOW_TOKEN = 'ntvhn7-9Ve8RfyO-cz5J_rX2-zU4-t49903487';
     const HOOK_URL = 'https://061731b2.ngrok.io/hook';
     const COMMANDS_PATH = __DIR__.'/../../BotCommands';
@@ -20,11 +18,15 @@ class BotService
      */
     private $api;
 
-    public function __construct()
-    {
-        $this->api = BotFactory::create(self::BOT_API_KEY);
+    /**
+     * BotService constructor.
+     * @param Telegram $telegram
+     */
+    public function __construct(
+        Telegram $telegram
+    ) {
+        $this->api = $telegram;
         TelegramLog::initialize(Logger::getLogger());
-        return $this;
     }
 
     /**
--- src/Bot/Telegram.php
+++ src/Bot/Telegram.php
@@ -8,18 +8,20 @@ use Longman\TelegramBot\Telegram as VendorTelegram;
 class Telegram extends VendorTelegram
 {
 
+    const BOT_API_KEY = '586818585:AAFz5J_rX2zU4fVe8RfyO3xVqwCr9N-FUZA';
+    const BOT_USERNAME = 'resonate.io';
+
+    public function __construct(
+        LectureProvider $lectureProvider
+    ) {
+        parent::__construct(self::BOT_API_KEY, self::BOT_USERNAME);
+        $this->lectureProvider = $lectureProvider;
+    }
+
     /**
      * @var LectureProvider
      */
     protected $lectureProvider;
-
-    /**
-     * @param LectureProvider $lectureProvider
-     */
-    public function setLectureProvider(LectureProvider $lectureProvider): void
-    {
-        $this->lectureProvider = $lectureProvider;
-    }
 
     /**
      * @return LectureProvider
